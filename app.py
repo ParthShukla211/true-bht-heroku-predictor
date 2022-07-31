398da469a4653ab54ee40eb8e8ecf55ad7539780
Parth Shukla <shuklaparth501@gmail.com>
2022-07-31 08:50:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import pickle
+import pandas as pd
+import numpy as np
+
+
+
+pipe = pickle.load(open('final-true-bht.pkl','rb'))
+st.title('True BHT Predictor')
+
+
+depth = st.number_input('depth')
+stemp = st.number_input('stemp')
+clay_present = st.number_input('clay present')
+clay_type = st.number_input('clay type')
+wa_or_nwa = st.number_input('wa or nwa')
+clay_content = st.number_input('clay content')
+corr_bht = st.number_input('corr bht')
+alpha = st.number_input('alpha')
+beta = st.number_input('beta')
+true_bht = st.number_input('true bht')
+
+
+# 'pH(CaCl2)':[pH(CaCl2)],'pH(H2O)':[pH(H2O)],
+if st.button('Predict Clay'):
+      input=pd.DataFrame({'depth':[depth],'stemp':[stemp],'clay_present':[clay_present],'clay_type':[clay_type],'wa_or_nwa':[wa_or_nwa],'clay_content':[clay_content],'corr_bht':[corr_bht],'alpha':[alpha],'beta':[beta],'true_bht':[true_bht]})
+      result = pipe.predict(input)
+      st.success('THE TRUE BHT FOR GIVEN DATA WILL BE {}'.format(result))
+# st.header(result)
